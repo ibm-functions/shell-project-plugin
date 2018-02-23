348bfab489c4124681b442ea1b037ca7ce0f813f
Lionel Villard <villard@us.ibm.com>
2018-02-23 14:34:51 -0500

    install bx wsk in travis

---
+++ tools/travis/bxsetup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ $BLUEMIX_ORG == "" ]]; then
+    echo "missing BLUEMIX_ORG. Aborting"
+    exit 1
+fi
+
+if [[ $BLUEMIX_API_KEY == "" ]]; then
+    echo "missing BLUEMIX_API_KEY. Aborting"
+    exit 1
+fi
+
+if [[ $BLUEMIX_SPACE == "" ]]; then
+    echo "missing BLUEMIX_SPACE. Aborting"
+    exit 1
+fi
+
+echo "installing bx"
+curl -fsSL https://clis.ng.bluemix.net/install/linux | sh
+
+echo "installing Cloud Functions plugin"
+bx plugin install Cloud-Functions -r Bluemix -f -v 1.0.6
+
+echo "login to bluemix"
+bx login -a api.ng.bluemix.net --apikey ${BLUEMIX_API_KEY} -o $BLUEMIX_ORG -s $BLUEMIX_SPACE
+
+echo "generation .wskprops"
+bx wsk property get
+
+
