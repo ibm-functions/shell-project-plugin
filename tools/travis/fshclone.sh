a2b1090ccbf89e1aac165295b27bd6b1ec67327e
Lionel Villard <villard@us.ibm.com>
2018-03-07 13:35:36 -0500

    move fsh install to sibling directory

--- tools/travis/fshclone.sh
+++ tools/travis/fshclone.sh
@@ -3,9 +3,9 @@
 set -e
 
 echo "cloning https://github.com/ibm-functions/shell.git"
-git clone https://github.com/ibm-functions/shell.git
+(cd $SHELL_DIR/.. && git clone https://github.com/ibm-functions/shell.git)
 
 echo "install shell"
-(cd shell/app && npm install)
+(cd $SHELL_DIR/app && npm install)
 
 echo "done"
--- tools/travis/plugininstall.sh
+++ tools/travis/plugininstall.sh
@@ -4,8 +4,8 @@ set -e
 
 echo "install project plugin"
 # fsh plugin install shell-project-plugin
-ln -s $TRAVIS_BUILD_DIR/. $TRAVIS_BUILD_DIR/shell/app/plugins/modules/shell-project-plugin
-(cd shell/dist && ./compile.js)
+ln -s $TRAVIS_BUILD_DIR/. $SHELL_DIR/app/plugins/modules/shell-project-plugin
+(cd $SHELL_DIR/dist && ./compile.js)
 
 echo "list plugin"
 fsh plugin list
